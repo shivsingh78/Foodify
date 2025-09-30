0b411ee0056d99526c216741731592cb087af442
shivsingh78 <shivsinghbaghel95@gmail.com>
2025-10-01 02:17:26 +0530

    feat:Add assests

--- frontend/src/components/UserDashboard.jsx
+++ frontend/src/components/UserDashboard.jsx
@@ -1,12 +1,22 @@
 import React, { useEffect } from 'react'
 import Nav from './Nav'
 import { useSelector } from "react-redux";
+import { categories } from '../category';
+import CategoryCard from './CategoryCard';
 
 function UserDashboard() {
 
   return (
     <div className='w-full min-h-screen bg-[#fff9f6] flex flex-col items-center '>
      <Nav/>
+     <div className='w-full max-w-6xl flex flex-col gap-5 items-start p-[10px]  '>
+      <h1 className='text-gray-800 text-2xl sm:text-3xl ' >Inspiration for your first order</h1>
+      <div className="w-full ">
+        {categories.map((cate,index)=>(
+          <CategoryCard data={cate} key={index} />
+        )) }
+      </div>
+     </div>
 
     </div>
   )
