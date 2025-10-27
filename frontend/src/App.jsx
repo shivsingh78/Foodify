c2a6d042efa93771a93b7ed9f082fa94b4b3885b
shiv singh baghel <shivsinghbaghel95@gmail.com>
2025-10-28 01:41:17 +0530

    Change server URL to production endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ import TrackOrderPage from './pages/TrackOrderPage';
 import Shop from './pages/Shop';
 import { io } from 'socket.io-client';
 import { setSocket } from './redux/userSlice';
-export const serverUrl="http://localhost:8000";
+export const serverUrl="https://foodify-backend-2qgx.onrender.com";
 
 function App() {
   const {userData} = useSelector(state=>state.user)
