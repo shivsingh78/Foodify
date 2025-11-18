a19fea8adb031ee1bb57cf83d5007fab68f69822
shivsingh78 <shivsinghbaghel95@gmail.com>
2025-11-18 13:59:48 +0530

    Update CORS origin to use FRONTEND_URL environment variable

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ if(process.env.NODE_ENV === "development") {
      app.use(morgan("dev"))
 }
 app.use(cors({
-     origin: process.env.CLIENT_URL,
+     origin: process.env.FRONTEND_URL,
      credentials:true,
 }))
 app.use(express.json())
