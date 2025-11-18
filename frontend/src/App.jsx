ea0c2d4494868ba3ea4c4b6c1ca4ebfdbf0a6a8a
shivsingh78 <shivsinghbaghel95@gmail.com>
2025-11-18 13:42:09 +0530

    Update server URLs to use environment variables for better configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ import TrackOrderPage from './pages/TrackOrderPage';
 import Shop from './pages/Shop';
 import { io } from 'socket.io-client';
 import { setSocket } from './redux/userSlice';
-export const serverUrl="http://localhost:8000";
+export const serverUrl= import.meta.env.VITE_API_URL;
 
 function App() {
   const {userData} = useSelector(state=>state.user)
--- frontend/src/category.js
+++ frontend/src/category.js
@@ -13,7 +13,7 @@ import image11 from "./assets/image11.jpg"
 export const categories = [
      {"category": "Snacks", "image": image1 },
      {"category": "Main Course", "image": image2 },
-     {"category": "Desserts", "image": image3 },
+     {"category": "Desserts", "image": image3 }, 
      {"category": "Pizza", "image": image4 },
      {"category": "Burgers", "image": image5 },
      {"category": "Sandwiches", "image": image6 },
